c429d11ff5ee1ddf2db2d1a1a38fc07b99228905
Nick Ubels <nickubels@gmail.com>
2019-04-23 10:53:14 +0200

    Ingeborg made code great again

--- script.py
+++ script.py
@@ -6,26 +6,36 @@ import os
 
 def get_args():
     parser = argparse.ArgumentParser(description='Train the network')
-    parser.add_argument('--gpu', '-g',action='store_true', default=False,
-        help="Whether the GPU should be used or not")
-    parser.add_argument('--epochs', '-e',metavar='INT', default=20,
-        help="Amount of epochs")
-    parser.add_argument('--optimizer', '-o',metavar='STRING', default="",
-        help="The optimizer you want to use")
-    parser.add_argument('--jobid','-j',metavar='STRING', default="",help="Jobid used for saving files")
+    parser.add_argument('--gpu', '-g' ,action='store_true', default=False, help="Whether the GPU should be used or not")
+    parser.add_argument('--epochs', '-e' ,metavar='INT', default=20, help="Amount of epochs")
+    parser.add_argument('--optimizer', '-o' ,metavar='STRING', default="", help="The optimizer you want to use")
+    parser.add_argument('--jobid' ,'-j' ,metavar='STRING', default="" , help="Jobid used for saving files")
     return parser.parse_args()
 
+
+class DeepLearningComparison:
+    def __init__(self):
+        self.args = get_args()
+
+        data_path = './data/'
+        if not os.path.exists(data_path):
+            os.makedirs(data_path)
+
+        # Loading the training data
+        trainset = torchvision.datasets.FashionMNIST(data_path, train=True, transform=None, target_transform=None, download=True)
+        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+        # Loading the test data
+        testset = torchvision.datasets.FashionMNIST(data_path, train=False, transform=None, target_transform=None, download=True)
+        self.testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=True, num_workers=2)
+
+        # Load a network
+        self.net = torchvision.models.inception_v3(pretrained=True)
+
+    def train_network(self):
+        print('value')
+
+
 if __name__ == '__main__':
-    args = get_args()
-
-    data_path = './data/'
-    if not os.path.exists(data_path):
-        os.makedirs(data_path)
-
-    # Loading the training data
-    trainset = torchvision.datasets.FashionMNIST(data_path, train=True, transform=None, target_transform=None, download=True)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
-
-    # Loading the test data
-    testset = torchvision.datasets.FashionMNIST(data_path, train=False, transform=None, target_transform=None, download=True)
-    testloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+    comparison = DeepLearningComparison()
+    comparison.train_network()

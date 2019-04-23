7f0715cade0df79107fbb03c77a0576c66b8ba6e
Nick Ubels <nickubels@gmail.com>
2019-04-23 10:31:42 +0200

    Loading data

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 import argparse
 import torch
+import torchvision
 import torch.nn as nn
+import os
 
 def get_args():
     parser = argparse.ArgumentParser(description='Train the network')
     parser.add_argument('--gpu', '-g',action='store_true', default=False,
-        help="Wether the GPU should be used or not")
+        help="Whether the GPU should be used or not")
     parser.add_argument('--epochs', '-e',metavar='INT', default=20,
         help="Amount of epochs")
     parser.add_argument('--optimizer', '-o',metavar='STRING', default="",
@@ -15,3 +17,15 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
+
+    data_path = './data/'
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+
+    # Loading the training data
+    trainset = torchvision.datasets.FashionMNIST(data_path, train=True, transform=None, target_transform=None, download=True)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+    # Loading the test data
+    testset = torchvision.datasets.FashionMNIST(data_path, train=False, transform=None, target_transform=None, download=True)
+    testloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)

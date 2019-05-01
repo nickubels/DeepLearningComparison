9c7e33a306d6fafea6f68f384f0ec1e902879aa8
Ingeborg <inge.keulen@gmail.com>
2019-05-01 12:14:50 +0200

    added legend

--- plot.py
+++ plot.py
@@ -4,9 +4,11 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def get_args():
     parser = argparse.ArgumentParser(description='Plot the results')
     parser.add_argument('--input_path', '-i', metavar='STRING', default='output', help="Where we find the input")
+    parser.add_argument('--log_path', '-l', metavar='STRING', default='logs', help="Where we find the logs")
     parser.add_argument('--output_path', '-o', metavar='STRING', default='output', help="Where we store the output")
     return parser.parse_args()
 
@@ -19,48 +21,73 @@ class Plotter(object):
         self.accuracy_files = [file for file in self.fileNames if 'accuracy.csv' in file]
         self.validation_files = [file for file in self.fileNames if 'val_loss.csv' in file]
         self.train_files = [file for file in self.fileNames if 'train_loss.csv' in file]
+        self.labels={}
 
     def plot_accuracy(self):
-        ### Loop over all files
+        # Loop over all files
         for file in self.accuracy_files:
+            # Obtain label
+            label = self.labels.get(file.split('_')[0], None)
 
-            ### Read .csv file and append to list
+            # Read .csv file and append to list
             df = pd.read_csv(os.path.join(self.args.input_path, file))
 
-            ### Create line for every file
-            plt.plot(df)
+            # Create line for every file
+            plt.plot(df, label=label)
 
-        ### Generate the plot
+        # Generate the plot
+        plt.legend()
         plt.show()
 
     def plot_validation(self):
-        ### Loop over all files
+        # Loop over all files
         for file in self.validation_files:
+            # Obtain label
+            label = self.labels.get(file.split('_')[0], None)
 
-            ### Read .csv file and append to list
+            # Read .csv file and append to list
             df = pd.read_csv(os.path.join(self.args.input_path, file))
 
-            ### Create line for every file
-            plt.plot(df)
+            # Create line for every file
+            plt.plot(df, label=label)
 
-        ### Generate the plot
+        # Generate the plot
+        plt.legend()
         plt.show()
 
     def plot_training(self):
-        ### Loop over all files
+        # Loop over all files
         for file in self.train_files:
+            # Obtain label
+            label = self.labels.get(file.split('_')[0], None)
 
-            ### Read .csv file and append to list
+            # Read .csv file and append to list
             df = pd.read_csv(os.path.join(self.args.input_path, file))
 
-            ### Create line for every file
-            plt.plot(df)
+            # Create line for every file
+            plt.plot(df, label=label)
 
-        ### Generate the plot
+        # Generate the plot
+        plt.legend()
         plt.show()
+
+    def obtain_labels(self):
+        files = os.listdir(self.args.log_path)
+
+        for log_file in files:
+            with open(os.path.join(self.args.log_path, log_file)) as f:
+                first_line = f.readline()
+                first_line = first_line.split()
+                job_id = [s for s in first_line if "job_id" in s]
+                job_id = job_id[0][8:-2]
+                optimizer = [s for s in first_line if "optimizer" in s]
+                optimizer = optimizer[0][11:-2]
+                self.labels[job_id] = optimizer
+
 
 def main():
     plotter = Plotter()
+    plotter.obtain_labels()
     plotter.plot_accuracy()
     plotter.plot_training()
     plotter.plot_validation()
@@ -68,4 +95,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-                
